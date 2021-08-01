26dd02d0abba7f57ff35b61e73c796dfb5573495
Steven Samy <stevensivo27@gmail.com>
2021-08-01 16:11:15 +0200

    check the code

--- todo.py
+++ todo.py
@@ -1,10 +1,10 @@
 # DB_HOST = "127.0.0.1"
 # DB_NAME = "steven1" 
 # DB_USER = "postgres"
-# DB_PASS = "JYPmcq?/5ByFm.S"
+# DB_PASS = "123"
 import psycopg2
 ## Initial database connection
-con = psycopg2.connect(host="127.0.0.1", dbname="steven1", user="postgres", password="JYPmcq?/5ByFm.S" )
+con = psycopg2.connect(host="127.0.0.1", dbname="steven1", user="postgres", password="123" )
 cur = con.cursor()
 
 ## Initial database tables
