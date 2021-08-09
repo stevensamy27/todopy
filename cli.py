b6d58bcca19070f8ba79ef91144c888c0bf9a41b
Steven Samy <stevensivo27@gmail.com>
2021-08-09 13:27:32 +0200

    tray something

--- cli.py
+++ cli.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 import sys, todo
-
-
 state_control = sys.argv
-
+    
 if len(state_control) == 2 and state_control[1] == "list":
     todos = todo.List()
     print(todos)
     
-elif len(state_control) >= 3 and state_control[1] == "create":
+elif len(state_control) >= 3 and state_control[1] == "create":       
     task = ' '.join(map(str, state_control[2:]))
     todo.Add(task)
    
--- todo.py
+++ todo.py
@@ -1,15 +1,19 @@
-
 import psycopg2
+import sys
 ## Initial database connection
 con = psycopg2.connect(host="localhost", dbname="steven3", user="postgres", password="JYPmcq?/5ByFm.S",port = "5432" )
 cur = con.cursor()
+state_control = sys.argv
 
-## Initial database tables
+
+## Initial database tables  
 
 cur.execute('''CREATE TABLE  IF NOT EXISTS todos
                (task text)''')
 con.commit()
 
+# columns = [column[0] for column in cur.execute ]
+# myset= [columns] +[row for row in cur.fetchal()]
 
 '''
 List: should list all tasks in the database
@@ -32,27 +36,38 @@ def List():
 Add: Should create a new task to the database given the input
 '''
 def Add(task):
-      # Logging
+    # Logging
+    
     print(f"Adding task [%s]" %task)
-    cur.execute ('SELECT * FROM todos')
-    for row in cur:
-        exit = ('%r' % (row,))
-        print(exit)
-        if row == task:
-            print("Invalid input!")
-        else:
-            print(f"Adding task [%s]" %task)
+    # cur.execute ('SELECT * FROM todos')
+
+    # for x in cur:
+    #     exit = ('%r' % (x,))
+    #     print(exit)
+    # if state_control in myset :
+    #     print("Invalid input!")
+    # else:   
+    #     print(f"Adding task [%s]" %task)
 
 
     # Database interaction
-    cur.execute ('SELECT * FROM todos')
-    for row in cur:
-        exit = ('%r' % (row,))
-        print(exit)
-        if row == task:
-            print("Invalid input!")
-        else:
-            print(f"Adding task [%s]" %task)
+    # cur.execute ('SELECT * FROM todos')
+    # for row in cur:
+    #     exit = ('%r' % (row,))
+    #     print(exit)
+    #     if row == task:
+    #         print("Invalid input!")
+    #     else:
+    #         print(f"Adding task [%s]" %task)
        
     con.commit()
     con.close()
+
+
+    
+# x = input()
+# colleection = ["sas","sas","sas","sas","sas","sas","saws","safs","ss","as","sa"]
+# if x in colleection:
+#     print("Invalid input!")
+# else:
+#      print("yes")             
