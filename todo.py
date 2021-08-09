21438abe0004c2bfc554502f0e0cf10bc56b71cd
Steven Samy <stevensivo27@gmail.com>
2021-08-09 19:16:15 +0200

    Now the code connect with database witheout errors

--- todo.py
+++ todo.py
@@ -1,5 +1,9 @@
+
 import psycopg2
 import sys
+
+
+
 ## Initial database connection
 con = psycopg2.connect(host="localhost", dbname="steven3", user="postgres", password="JYPmcq?/5ByFm.S",port = "5432" )
 cur = con.cursor()
@@ -10,10 +14,7 @@ state_control = sys.argv
 
 cur.execute('''CREATE TABLE  IF NOT EXISTS todos
                (task text)''')
-con.commit()
-
-# columns = [column[0] for column in cur.execute ]
-# myset= [columns] +[row for row in cur.fetchal()]
+con.commit() 
 
 '''
 List: should list all tasks in the database
@@ -36,38 +37,17 @@ def List():
 Add: Should create a new task to the database given the input
 '''
 def Add(task):
-    # Logging
-    
-    print(f"Adding task [%s]" %task)
-    # cur.execute ('SELECT * FROM todos')
-
-    # for x in cur:
-    #     exit = ('%r' % (x,))
-    #     print(exit)
-    # if state_control in myset :
-    #     print("Invalid input!")
-    # else:   
-    #     print(f"Adding task [%s]" %task)
-
-
-    # Database interaction
-    # cur.execute ('SELECT * FROM todos')
-    # for row in cur:
-    #     exit = ('%r' % (row,))
-    #     print(exit)
-    #     if row == task:
-    #         print("Invalid input!")
-    #     else:
-    #         print(f"Adding task [%s]" %task)
-       
+    x = input()
+    cur = con.cursor()
+    cur.execute("select task from todos ")
+    data = cur.fetchall()
+    print(data)
+    if x in  data :
+        print("yes")
+        # print(f"Adding task ('%s')" %x)
+        # cur.execute("insert INTO todos(task) values(%s)" ,x)
+    else:
+        print("Invaled Input")
     con.commit()
+    cur.close()
     con.close()
-
-
-    
-# x = input()
-# colleection = ["sas","sas","sas","sas","sas","sas","saws","safs","ss","as","sa"]
-# if x in colleection:
-#     print("Invalid input!")
-# else:
-#      print("yes")             
